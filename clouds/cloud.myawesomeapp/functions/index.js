2544d0157014f8132c5c8f651803ddc96405b5ed
hi-matbub <6matbub@gmail.com>
2022-04-30 09:05:02 -0700

    cloud server running on local emulator

--- clouds/cloud.myawesomeapp/functions/index.js
+++ clouds/cloud.myawesomeapp/functions/index.js
@@ -1,15 +1,17 @@
 const functions = require("firebase-functions");
+const express = require('express');
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-
-const express = require("express");
 const app = express();
+app.get('/hello-world', (req,res) => {
+  res.json('hello from the whole world!')
+})
 
-app.get("/hello-world", ((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-}));
+exports.app = functions.https.onRequest(app);
 
-exports.app;
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
