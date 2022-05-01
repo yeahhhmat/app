a1ac60a61b7bb86d17fcbb07dc2d8a245dfa6c44
hi-matbub <6matbub@gmail.com>
2022-04-30 20:26:05 -0700

    configure eslint within cloud.myawesomeapp

--- clouds/cloud.myawesomeapp/functions/index.js
+++ clouds/cloud.myawesomeapp/functions/index.js
@@ -1,24 +1,17 @@
-const functions = require("firebase-functions");
-// const firebase = require("firebase-admin");
-const { initializeApp } = require("firebase/app");
-const { getDatabase, set, ref } = require("firebase/database");
-
+const functions = require('firebase-functions');
+const { initializeApp } = require('firebase/app');
+const { getDatabase, set, ref } = require('firebase/database');
 const express = require('express');
-
-// const { ref } = require("firebase-functions/v1/database");
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
-
 const app = express();
 const firebaseApp = initializeApp({
-  apiKey: "AIzaSyBtdH5nFEr8biBPQNC9M1ho6D7KYb6BE1s",
-  authDomain: "myawesomeappdotorg.firebaseapp.com",
-  databaseURL: "https://myawesomeappdotorg-default-rtdb.firebaseio.com",
-  projectId: "myawesomeappdotorg",
-  storageBucket: "myawesomeappdotorg.appspot.com",
-  messagingSenderId: "781395862437",
-  appId: "1:781395862437:web:89d9bf74111cd8fe673e4c"
-})
+  apiKey: 'AIzaSyBtdH5nFEr8biBPQNC9M1ho6D7KYb6BE1s',
+  authDomain: 'myawesomeappdotorg.firebaseapp.com',
+  databaseURL: 'https://myawesomeappdotorg-default-rtdb.firebaseio.com',
+  projectId: 'myawesomeappdotorg',
+  storageBucket: 'myawesomeappdotorg.appspot.com',
+  messagingSenderId: '781395862437',
+  appId: '1:781395862437:web:89d9bf74111cd8fe673e4c'
+});
 
 const database = getDatabase(firebaseApp);
 
@@ -28,14 +21,9 @@ function example() {
   });
 }
 app.get('/hello-world', (req,res) => {
-  res.set('Cache-Control', 'public, max-age=300, s-maxage=600')
-  
-  example()
-  res.json( 'hello from the whole world!')
-  
-})
-
-
+  res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+  example();
+  res.json( 'hello from the whole world!');
+});
 
 exports.app = functions.https.onRequest(app);
-
